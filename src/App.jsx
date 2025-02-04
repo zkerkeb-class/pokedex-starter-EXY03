251e0fc051dcc76b6415ebb9b619ac8841c489c9
EXY03 <3ric.xue@gmail.com>
2025-02-04 16:52:07 +0100

    console log test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import pokemons from './assets/pokemons'
+
+console.log("test log")
+console.log("🚀 ~ pokemons:", pokemons)
+
+const bulbasaur = pokemons[0]
+console.log("🚀 ~ bulbasaur:", bulbasaur)
+console.log("🚀 ~ bulbasaur:", bulbasaur.type[0])
+console.log("🚀 ~ bulbasaur:", bulbasaur.name.japanese)
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +18,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
