036ade78e3e6419bc5a0d411c13f338ba70e1278
EXY03 <3ric.xue@gmail.com>
2025-02-20 11:15:27 +0100

    Ajout css
    Searchbar
    pokemon list bien

--- src/assets/components/PokedexNavigation.jsx
+++ src/assets/components/PokedexNavigation.jsx
@@ -21,13 +21,7 @@ function PokedexNavigation() {
       <header>
         <h1>Pokédex</h1>
       </header>
-      <input
-        type="text"
-        placeholder="Rechercher un Pokémon..."
-        value={searchTerm}
-        onChange={handleSearch}
-        className="search-bar"
-      />
+      <input type="text" placeholder="Rechercher un Pokémon..." value={searchTerm} onChange={handleSearch} className="search-bar" />
       <div className="pokemon-list">
         {filteredPokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
